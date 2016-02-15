f610e912f627cc426e559cfba8a0d1297b2325d1
Winand <winand.seldeslachts@hotmail.com>
2016-02-15 12:48:02 +0100

    added main coding file function:  * extract signals from given wavedrom JSON output  * read and clean testbench template  * translate first wavedrom signal to VHDL and add to testbench  * write changes to output VHDL testbench file  (* open output file in gedit)

---
+++ Code/main.py
@@ -0,0 +1,28 @@
+import read_json
+import generate_testbench
+import json_to_vhdl
+
+# - import wavedrom input, extract signals from raw data and print them
+testfile = read_json.open_json_file("json_resources/testfile.json")
+signals = read_json.extract_signals(testfile)
+# read_json.print_signals(signals)
+
+
+# - read the testbench template
+vhdl_testbench = generate_testbench.read_template()
+
+
+# - clean the template
+vhdl_testbench = generate_testbench.clean_testbench_template(vhdl_testbench)
+
+
+# - translate a signal to vhdl and add it to the testbench
+signal_declaration = json_to_vhdl.translate(signals[0])
+vhdl_testbench = generate_testbench.add_signal_declaration(vhdl_testbench, signal_declaration)
+
+
+# - write changes to vhdl testbench
+generate_testbench.write_output(vhdl_testbench)
+
+# - open output file
+# generate_testbench.open_output()
