9569a97cdbb5fa1915d805a861dca9cfa7a235c8
Winand <winand.seldeslachts@hotmail.com>
2016-02-15 17:40:24 +0100

    added wavedrom example for testing

--- Code/main.py
+++ Code/main.py
@@ -3,7 +3,7 @@ import generate_testbench
 import json_to_vhdl
 
 # - import wavedrom input, extract signals from raw data and print them
-testfile = read_json.open_json_file("json_resources/testfile.json")
+testfile = read_json.open_json_file("json_resources/andgate.json")
 signals = read_json.extract_signals(testfile)
 # read_json.print_signals(signals)
 
@@ -17,9 +17,19 @@ vhdl_testbench = generate_testbench.clean_testbench_template(vhdl_testbench)
 
 
 # - translate a signal to vhdl and add it to the testbench
-signal_declaration = json_to_vhdl.translate(signals[0])
-vhdl_testbench = generate_testbench.add_signal_declaration(vhdl_testbench, signal_declaration)
+# for j in range(0, len(signals[0])):
+#     clk_declaration = json_to_vhdl.translate(signals[0][j])
+#     vhdl_testbench = generate_testbench.add_clock_signal_declaration(vhdl_testbench, clk_declaration)
+#
+# for j in range(0, len(signals[1])):
+#     input_declaration = json_to_vhdl.translate(signals[1][j])
+#     vhdl_testbench = generate_testbench.add_input_signal_declaration(vhdl_testbench, input_declaration)
+#
+# for j in range(0, len(signals[2])):
+#     output_declaration = json_to_vhdl.translate(signals[2][j])
+#     vhdl_testbench = generate_testbench.add_output_signal_declaration(vhdl_testbench, output_declaration)
 
+vhdl_testbench = generate_testbench.set_up_uut(vhdl_testbench)
 
 # - write changes to vhdl testbench
 generate_testbench.write_output(vhdl_testbench)
