700e46687cdc6cf17549b12ac6f7cdf98ec0624f
Winand <winand.seldeslachts@hotmail.com>
2016-02-15 12:44:17 +0100

    added file to generate VHDL testbench funtion:  * read template and clean comments from it  * add signal declaration to testbench  * write output to VHDL testbench file  * open output file in gedit (linux)

---
+++ Code/generate_testbench.py
@@ -0,0 +1,45 @@
+import os
+import re
+
+testbench_template = 'template_TB.vhd'
+output_file = 'testbench_out.vhd'
+
+def read_template():
+    with open(testbench_template, 'r') as template_file:
+            template = template_file.read()
+    template_file.close()
+    return template
+
+
+def write_output(data):
+    with open(output_file, 'w') as template_file:
+            template_file.write(data)
+    template_file.close()
+
+
+def open_output():
+    os.system("gedit " + output_file)
+
+
+def clean_testbench_template(template):
+    pattern = re.compile('--/ ')  # a pattern for a number
+    print pattern
+    # TODO: make this better
+    for x in range(0,15):
+        for match in re.finditer(pattern, template):
+            s = match.start()
+            pattern2 = re.compile('/--')  # a pattern for a number
+            for match2 in re.finditer(pattern2, template):
+                e = match2.end()
+                break
+            print "match found!"
+            print template[s:e]
+            # template[s:e+1].rstrip()
+            # template = template.replace(template[s:e],template[s:e+1].rstrip())
+            template = template.replace(template[s:e],"")
+            break
+    return template
+
+
+def add_signal_declaration(template, signal_decl):
+    return template.replace("--# Inputs","--# Inputs \n" + signal_decl)
