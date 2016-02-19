848a452922222e0248db847a8389d0489d2a0de2
Winand <winand.seldeslachts@hotmail.com>
2016-02-19 15:52:36 +0100

    Major changes:  * made write_output create file if not already there  * made open output open sigasi IDE for vhd files  * made set_up_uut do following things:      * change entity name to given name (derived from JSON source file name)      * set up the component declaration (port declaration) for the testbench      * set up the port map for the testbench      * all of the above is derived from the JSON input file  * added set_up_signals to add signal declaration to file (derived from JSON input file)  * removed old signal declaration adding methods  * added set_up_stimulus_process method that generates the actual VHDL signal drivers for this test  * added set_up_check process method that generates the actual VHDL checks for this test  * made generate_testbench to combine setting up the uut, the driver signals and changing the test name

--- Code/generate_testbench.py
+++ Code/generate_testbench.py
@@ -1,9 +1,10 @@
 import os
 import re
 
-testbench_template = 'template_TB.vhd'
-file_to_test = 'file_to_test.vhd'
-output_file = 'testbench_out.vhd'
+testbench_template = 'vhdl_files/template_TB.vhd'
+# file_to_test = 'vhdl_files/src/file_to_test.vhd'
+output_path = 'vhdl_files/test/tb_'
+
 
 def read_template():
     with open(testbench_template, 'r') as template_file:
@@ -12,21 +13,22 @@ def read_template():
     return template
 
 
-def read_file_to_test():
-    with open(file_to_test, 'r') as template_file:
-            template = template_file.read()
-    template_file.close()
-    return template
+# def read_file_to_test():
+#     with open(file_to_test, 'r') as template_file:
+#             template = template_file.read()
+#     template_file.close()
+#     return template
 
 
-def write_output(data):
-    with open(output_file, 'w') as template_file:
+def write_output(data, filename):
+    with open(output_path + filename + ".vhd", 'w') as template_file:
             template_file.write(data)
     template_file.close()
 
 
-def open_output():
-    os.system("gedit " + output_file)
+def open_output(filename):
+    os.system("/opt/sigasi/sigasi " + output_path + filename + ".vhd")
+    # os.system("gedit " + output_file)
 
 
 def clean_testbench_template(template):
@@ -39,40 +41,57 @@ def clean_testbench_template(template):
             for match2 in re.finditer(pattern2, template):
                 e = match2.end()
                 break
-            # print "match found!"
-            # print template[s:e]
             template = template.replace(template[s:e+1], "")
             break
     return template
 
 
-def set_up_uut(testbench):
-    ftt = read_file_to_test()
-    s = ftt.find("entity") + len("entity")
-    e = ftt.find("is", s)
-    name = ftt[s:e]
-    testbench = testbench.replace("COMPONENT uut", "COMPONENT " + name + '\n')
+def set_up_uut(testbench, entity_name, port_declaration, port_map, filename):
+    testbench = testbench.replace("entity_name", filename)
+    testbench = testbench.replace("COMPONENT uut", "COMPONENT " + entity_name + '\n')
+    testbench = testbench.replace("UUT PORT MAP", entity_name + " PORT MAP")
 
-    testbench = testbench.replace("UUT PORT MAP", name + "PORT MAP")
-
-    rep = ftt[ftt.find("port(")+len("port("):ftt.find(");")+2]
-    testbench = testbench.replace("PORT();", "PORT(" +rep + '\n')
+    testbench = testbench.replace("PORT();", port_declaration + "\n")
+    testbench = testbench.replace("PORT MAP();", port_map + "\n")
 
     return testbench
 
-def get_all_ports_from_vhdl(port_list):
-    for i in range(0, len(port_list)):
-        a = port_list[i]
-#     TODO: get all the ports and directions from the port list
+
+def set_up_signals(testbench, signal_declarations):
+    signal_types = ["Clock", "Input", "Output"]
+    i = 0
+    for type in signal_declarations:
+        if len(type) > 0:
+            for signal_decl in type:
+                find = "--# " + signal_types[i] + " Signals"
+                replace = "--# " + signal_types[i] + " Signals \n" + signal_decl
+                testbench = testbench.replace(find, replace)
+        i += 1
+    return testbench
 
 
-def add_clock_signal_declaration(template, signal_decl):
-    return template.replace("--# Clock Signal", "--# Clock Signal \n" + signal_decl)
+def set_up_stimulus_process(testbench, test_name, input_signals):
+    stimulus = ""
+    for signal in input_signals:
+        wave = signal["wave"]
+        stimulus += "sig_" + signal["name"] + " <= '" + wave[0] + "';\n"
+    # find = "--# " + test_name + "stimulus"
+    find = "--# test stimulus"
+    return testbench.replace(find, stimulus)
 
 
-def add_input_signal_declaration(template, signal_decl):
-    return template.replace("--# Input Signals", "--# Input Signals \n" + signal_decl)
+def set_up_check_process(testbench, test_name, output_signals):
+    checking = ""
+    for signal in output_signals:
+        wave = signal["wave"]
+        checking += "check( sig_" + signal["name"] + " = '" + wave[0] + "', \" Expected sig_" + signal["name"] + " = '" \
+                    + wave[0]+ "' at this point\");"
+    # find = "--# " + test_name + "checking"
+    find = "--# test checking"
+    return testbench.replace(find, checking)
 
 
-def add_output_signal_declaration(template, signal_decl):
-    return template.replace("--# Output Signals", "--# Output Signals \n" + signal_decl)
+def generate_testbench(testbench, entity_name, port_declaration, port_map, signal_declarations, filename, test_name):
+    testbench = testbench.replace("test_name", test_name)
+    testbench = set_up_uut(testbench, entity_name, port_declaration, port_map, filename)
+    return set_up_signals(testbench, signal_declarations)
