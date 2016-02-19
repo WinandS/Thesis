19525d690ec4a280f7ef74fab9df17338ed4f8de
Winand <winand.seldeslachts@hotmail.com>
2016-02-19 15:58:41 +0100

    simplified some methods added extract_info method to extract all relevant info from JSON file improved print function

--- Code/read_json.py
+++ Code/read_json.py
@@ -3,8 +3,8 @@ import glob
 
 
 # get input data from file
-def open_json_file(name):
-    for filename in glob.glob(name):
+def open_json_file(filename):
+    for filename in glob.glob(filename):
         with open(filename) as data_file:
             data = json.load(data_file)
             return data
@@ -15,9 +15,9 @@ def extract_signals(data):
     clk_signal = []
     input_signals = []
     output_signals = []
-    for i in range(0,len(data["signal"])):
-        if len(data["signal"][i]) is not 0:
-            signal = data["signal"][i]
+
+    for signal in data["signal"]:
+        if len(signal) is not 0:
 
             if signal[0] == "CLK":
                 clk_signal = signal[1]
@@ -30,14 +30,26 @@ def extract_signals(data):
                 for j in range(1, len(signal)):
                     output_signals.append(signal[j])
 
-    return [[clk_signal], input_signals, output_signals]
+    return [clk_signal, input_signals, output_signals]
+
+
+# extract all relevant info from json file
+def extract_info(filename):
+    data = open_json_file(filename)
+    return data["name"], data["test"], extract_signals(data)
 
 
 # print extracted wavedrom signals
 def print_signals(signals):
-    for j in range(0, len(signals)):
-        for i in range(0, len(signals[j])):
-            print "-------------", signals[j][0]["name"], "---------------"
-            for element in signals[j][0]:
-                print element, " : ", signals[j][0][element]
-            print
+    signal_types = ["CLK", "IN", "OUT"]
+    i = 0
+    for signal_type in signals: # there's three types of signals: CLK, IN and OUT.
+        if len(signal_type) > 0:
+            print "----------------------------------------------------------------------"
+            print "-----------------------           " + signal_types[i] + "             --------------------"
+            print "----------------------------------------------------------------------"
+            for signal in signal_type:
+                print "-------------", signal["name"], "---------------"
+                for element in signal:
+                    print element, " : ", signal[element]
+        i += 1
