8cae7dabb04ae2503463cbece2c694c3ef6c9fbe
Winand <winand.seldeslachts@hotmail.com>
2016-02-19 15:55:08 +0100

    simplified generate_signal_declaration; added generate_vhdl method to create VHDL code from JSON input and returns processed info

--- Code/json_to_vhdl.py
+++ Code/json_to_vhdl.py
@@ -1,6 +1,37 @@
-# Generates all necessary VHDL code for given signal in json format. (only signal declaration for now)
-def translate(json_signal):
+# Generates VHDL signal declaration for given signal in json format.
+def generate_signal_declaration(json_signal):
+    # print json_signal
     signal_name = json_signal["name"]
-    signal_declaration = "    signal " + signal_name + " : std_logic := '0';"
-    # signal_waveform = json_signal["wave"]
-    return signal_declaration
+    signal_type = json_signal["type"]
+    return "    signal sig_" + signal_name + " : " + signal_type + ";"
+
+
+# Generates signal and PORT declaration and port map for given signal set.
+def generate_vhdl(json_signal_set):
+    i = 0
+    signal_types = ["clk", "in", "out"]
+    port_declaration = "PORT(\n"
+    port_map = "PORT MAP(\n"
+    signal_declarations = [[], [], []]
+    first = True
+    for signal_type in json_signal_set: # there's three types of signals: CLK, IN and OUT.
+                                        # (clk is a special case of input signal)
+        if len(signal_type) > 0:
+            direction = signal_types[i]
+            for signal in signal_type:
+                if first:
+                    new_port = signal["name"] + " : " + direction + " " + signal["type"]
+                    new_map = signal["name"] + " => sig_" + signal["name"]
+                    first = False
+                else:
+                    new_port = ";\n" + signal["name"] + " : " + direction + " " + signal["type"]
+                    new_map = ",\n" + signal["name"] + " => sig_" + signal["name"]
+                port_declaration += new_port
+                port_map += new_map
+
+                signal_decl = generate_signal_declaration(signal)
+                signal_declarations[i].append(signal_decl)
+        i += 1
+    port_declaration += " );"
+    port_map += " );"
+    return port_declaration, port_map, signal_declarations
